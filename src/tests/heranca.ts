bb229ae9616886178a239e7986b99c7e82e92d47
Nicolas Soares Henrique <nicolas.soaresh.98@gmail.com>
2023-09-04 21:31:34 -0300

    implementado carrinho de compras

--- src/tests/heranca.ts
+++ src/tests/heranca.ts
@@ -14,7 +14,7 @@ class Animal1 {
   }
 }
 
-class Cachorro extends Animal {
+class Cachorro extends Animal1 {
   puroSangue: boolean;
   constructor(raca: string, nome: string, quantosOlhos: number, puroSangue: boolean) {
     super(raca, nome, quantosOlhos);
@@ -26,7 +26,7 @@ class Cachorro extends Animal {
   }
 }
 
-class Gato extends Animal {
+class Gato extends Animal1 {
   vacinado: boolean;
 
   constructor(raca: string, nome: string, quantosOlhos: number, vacinado: boolean) {
@@ -35,7 +35,7 @@ class Gato extends Animal {
   }
 }
 
-let a = new Animal("seila", "Augusto", 2);
+let a = new Animal1("seila", "Augusto", 2);
 
 let c = new Cachorro("Golden", "Tiozinho", 10, false);
 c.fazerBarulho();
@@ -44,7 +44,7 @@ let g1 = new Gato("Vira lata 1", "Mingau", 80, true);
 let g2 = new Gato("Vira lata 2", "Rogerinho", 1, false);
 let g3 = new Gato("Vira lata 3", "Ronaldo", 2, true);
 
-function aux2(animal: Animal) {
+function aux2(animal: Animal1) {
   animal.fazerBarulho();
   console.log(animal.nome, animal.quantosOlhos, animal.raca);
 }
